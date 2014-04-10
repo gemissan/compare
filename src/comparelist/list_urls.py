2b78085053222cf94237a048247b3743138259f4
Grzegorz Świca <gemissan@gmail.com>
2014-04-10 23:47:27 +0200

    Changed comparelist list linking

---
+++ src/comparelist/list_urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, url
+
+
+urlpatterns = patterns("comparelist.views",
+    url(r"(?P<list_id>\d+)$", "show", name="show-list-view"),
+    url(r"create$", "create", name="create-list-view"),
+    url(r"", "index", name="index-list-view")
+)
