abcc906f64fb88ecb003624c35ea3be99aff921d
Grzegorz Świca <gemissan@gmail.com>
2014-04-10 23:38:34 +0200

    Changed compareuser links

--- src/comparemain/urls.py
+++ src/comparemain/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns("comparemain.views",
-    url(r"^$", "index", name="index_view"),
-    url(r"^about$", "about", name="about_view"),
+    url(r"login$", "login", name="login-view"),
+    url(r"logout$", "logout", name="logout-view"),
+    url(r"create$", "create", name="create-user-view"),
+    url(r"^about$", "about", name="about-view"),
+    url(r"^$", "index", name="index-view"),
 )
--- src/compareuser/urls.py
+++ src/compareuser/urls.py
@@ -2,8 +2,5 @@ from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns("compareuser.views",
-    url(r"login$", "login", name="login_user_view"),
-    url(r"logout$", "logout", name="logout_user_view"),
-    url(r"create$", "create", name="create_user_view"),
-    url(r"(?P<user_id>\d+)$", "show", name="show_user_view")
+    url(r"(?P<user_slug>\w+)$", "show", name="show-user-view")
 )
