ce177aa3a944e77fabd624f58583faae29e2a669
Grzegorz Świca <gemissan@gmail.com>
2014-04-11 00:04:18 +0200

    Changed main linking

--- src/Compare/urls.py
+++ src/Compare/urls.py
@@ -8,5 +8,7 @@ admin.autodiscover()
 urlpatterns = patterns("",
     url(r"^admin", include(admin.site.urls)),
     url(r"^user/", include("compareuser.urls")),
+    url(r"^list/", include("comparelist.list_urls")),
+    url(r"^view/", include("comparelist.view_urls")),
     url(r"^youtube/", include("compareyoutube.urls")),
     url(r"", include("comparemain.urls")))
