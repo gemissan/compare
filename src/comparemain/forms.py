09dfbcc5da49a4cfb5bac31ae10ec5d33d39074d
Grzegorz Świca <gemissan@gmail.com>
2014-04-15 08:37:49 +0200

    Added css assets to login form

--- src/comparemain/forms.py
+++ src/comparemain/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django import forms
 
 from django.contrib.auth import get_user_model
 
@@ -6,7 +6,27 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class LoginForm(ModelForm):
+class EmailField(forms.TextInput):
+    
+    class Media:
+        css = {
+            "all": ("css/forms.css",)
+        }
+        
+        
+class PasswordField(forms.PasswordInput):
+    
+    class Media:
+        css = {
+            "all": ("css/forms.css",)
+        }
+
+
+class LoginForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ["email", "password"]
+        widgets = {
+            "email": EmailField(attrs={"class": "login_input"}),
+            "password": PasswordField(attrs={"class": "login_input password_input"})
+        }
