28cb84237091b63ef28fa1a12eb06b5e8ec80bdd
Grzegorz Świca <gemissan@gmail.com>
2014-04-16 08:22:36 +0200

    Added type field to Message

--- src/comparemessages/models.py
+++ src/comparemessages/models.py
@@ -5,7 +5,18 @@ from comparemessages.managers import MessageManager
 
 class Message(models.Model):
     
+    INFO = "IF"
+    WARNING = "WR"
+    ERROR = "ER"
+    
+    TYPE_CHOICES = (
+        (INFO, "Info"),
+        (WARNING, "Warning"),
+        (ERROR, "Error"),
+    )
+    
     name = models.CharField(max_length=20, primary_key=True)
+    type = models.CharField(max_length=2, choices=TYPE_CHOICES)
     message = models.CharField(max_length=255)
     
     class Meta:
