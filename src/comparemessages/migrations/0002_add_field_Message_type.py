5f3a8fa6afc3f4406d434f14bd0f86512d078e36
Grzegorz Świca <gemissan@gmail.com>
2014-04-16 08:22:49 +0200

    Added schemamigration for Message

---
+++ src/comparemessages/migrations/0002_add_field_Message_type.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Message.type'
+        db.add_column(u'comparemessages_message', 'type',
+                      self.gf('django.db.models.fields.CharField')(default=None, max_length=2),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Message.type'
+        db.delete_column(u'comparemessages_message', 'type')
+
+
+    models = {
+        u'comparemessages.message': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Message'},
+            'message': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '20', 'primary_key': 'True'}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '2'})
+        }
+    }
+
+    complete_apps = ['comparemessages']
