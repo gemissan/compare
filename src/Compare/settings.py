127b1f6dae4ec8a2225074db4c640a56d4da29d0
Grzegorz Świca <gemissan@gmail.com>
2014-04-17 08:00:27 +0200

    Removed messages middleware

--- src/Compare/settings.py
+++ src/Compare/settings.py
@@ -105,7 +105,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'comparemessages.middleware.ShowMessagesMiddleware',
 )
 
 ROOT_URLCONF = 'Compare.urls'
