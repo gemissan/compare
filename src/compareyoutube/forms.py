7063042d0333812fc8e61082728c42ee491f0e55
Grzegorz Świca <gemissan@gmail.com>
2014-04-24 21:02:26 +0200

    Tweeked add new youtube form

--- src/compareyoutube/forms.py
+++ src/compareyoutube/forms.py
@@ -3,7 +3,19 @@ from django import forms
 from compareyoutube.models import YoutubeObject
 
 
-class AddObjectForm(forms.ModelForm):
+class YoutubeUrlField(forms.forms.TextInput):
     
-    class Meta:
-        model = YoutubeObject
+    class Media:
+        css = {
+            "all": ("css/forms.css", "css/youtube.css",)
+        }
+
+
+class AddObjectForm(forms.Form):
+    
+    url = forms.CharField(widget=YoutubeUrlField(attrs={"class": "url_input youtube_url_input"}))
+        
+    class Media:
+        css = {
+            "all": ("css/forms.css", "css/youtube.css",)
+        }
