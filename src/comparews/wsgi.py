0931aa6e00d3e3ec6aa8687a270072bd302c338e
Grzegorz Świca <gemissan@gmail.com>
2014-04-29 09:06:02 +0200

    Cleaned up wsgi.py for comparews

--- src/comparews/wsgi.py
+++ src/comparews/wsgi.py
@@ -1,35 +1,45 @@
 import os
 import sys
+import logging
 
 import tornado.websocket
 import tornado.web
 import tornado.wsgi
 
 
+logger = logging.getLogger("ws")
+
+
 sys.path = ["."] + sys.path
 os.environ["DJANGO_SETTINGS_MODULE"] = "Compare.settings"
 
     
-class WebSocketHandler(tornado.websocket.WebSocketHandler):
+class CompareYoutubeWSHandler(tornado.websocket.WebSocketHandler):
+    
     def open(self):
-        self.write_message("Hello")
+        logger.debug("socket opened")
       
     def on_message(self, message):
-        pass
- 
+        logger.debug("message %s received")
+        self.write_message(u"You said: " + message)
+        
     def on_close(self):
-        pass
-  
-  
+        logger.debug("socket closed")
+        
+        
+urls = [
+    (r"/youtube", CompareYoutubeWSHandler),
+]
 
-class HelloHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello")
-  
 
-application = tornado.wsgi.WSGIApplication(
-    [
-     (r"/ws/hello", HelloHandler),
-     (r"/ws", WebSocketHandler),
-     ])
+# For testing
+if __name__ == "__main__":
+    import tornado.ioloop
+    
+    application = tornado.web.Application(urls)
 
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
+else:
+    application = tornado.wsgi.WSGIApplication(urls)
+    
